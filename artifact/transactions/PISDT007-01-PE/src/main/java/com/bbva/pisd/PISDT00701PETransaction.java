63a5f01272e31ff9433cd0c8fc6f94eebf73a780
paul.rivera <paul.rivera@bbva.com>
2021-06-17 01:29:46 -0500

    Modify Output

--- artifact/transactions/PISDT007-01-PE/src/main/java/com/bbva/pisd/PISDT00701PETransaction.java
+++ artifact/transactions/PISDT007-01-PE/src/main/java/com/bbva/pisd/PISDT00701PETransaction.java
@@ -47,8 +47,8 @@ public class PISDT00701PETransaction extends AbstractPISDT00701PETransaction {
 		FinancingPlanDTO output = pisdr030.executeSimulateInsuranceQuotationInstallmentPlan(input);
 
 		if(output != null) {
-			this.setStartdate(output.getStartDate().toDate());
-			this.setMaturitydate(output.getMaturityDate().toDate());
+			this.setStartdate(output.getStartDate().toDateTimeAtCurrentTime().toDate());
+			this.setMaturitydate(output.getMaturityDate().toDateTimeAtCurrentTime().toDate());
 			this.setInstallmentplans(output.getInstallmentPlans());
 			this.setHttpResponseCode(HttpResponseCode.HTTP_CODE_200, Severity.OK);
 		} else {
