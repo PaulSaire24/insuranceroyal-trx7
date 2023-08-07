c6ca4df4df5b8f868580c5e46983d2d62348c064
Aaron <carlos.carrillo.delgado@bbva.com>
2023-08-07 18:51:37 -0500

    feat: PERAR-24008 PERAR-24383 Update version lib12

--- artifact/libraries/PISDR030IMPL/src/main/java/com/bbva/pisd/lib/r030/impl/factory/FactoryRequestQuotation.java
+++ artifact/libraries/PISDR030IMPL/src/main/java/com/bbva/pisd/lib/r030/impl/factory/FactoryRequestQuotation.java
@@ -3,12 +3,14 @@ package com.bbva.pisd.lib.r030.impl.factory;
 import com.bbva.elara.configuration.manager.application.ApplicationConfigurationService;
 import com.bbva.pisd.lib.r030.impl.util.Constants;
 
+import java.util.Objects;
+
 public class FactoryRequestQuotation {
 
     private FactoryRequestQuotation(){}
 
     public static RequestSchedule getRequestRimac(String insuranceBusinessName, String productShortDesc, ApplicationConfigurationService applicationConfigurationService){
-        if(insuranceBusinessName.equals(Constants.BUSINESS_NAME_VIDA)){
+        if(Objects.nonNull(insuranceBusinessName) && insuranceBusinessName.equals(Constants.BUSINESS_NAME_VIDA)){
             return new RequestLife(productShortDesc,applicationConfigurationService);
         }else{
             return new RequestNoLife(applicationConfigurationService);
